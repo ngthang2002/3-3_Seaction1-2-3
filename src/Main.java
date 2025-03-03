4a329edacfeeaa510fbcfe7a75a121495a7e4645
ngthang2002 <ngthang200201@gmail.com>
2025-03-03 10:39:15 +0700

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Byte: ");
+        byte byteNumber = scanner.nextByte();
+        System.out.print("Short: ");
+        short shortNumber = scanner.nextShort();
+        System.out.print("Integer: ");
+        int intNumber = scanner.nextInt();
+        scanner.close();
+        long longNumber = (long) 50000 + (long) 10 * (byteNumber + shortNumber + intNumber);
+        System.out.println("Long = 50_000 * 10 * (Byte + Short + Integer): " + (50000 + 10 * (byteNumber + shortNumber + intNumber)));
+        System.out.println(longNumber);
     }
 }
